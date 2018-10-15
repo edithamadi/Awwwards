61890d62b5f67d7b5e3c48f13ef197801da940e0
Edith <you@example.com>
2018-10-15 15:45:39 +0300

    added function for search

--- ap/views.py
+++ ap/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profile, Projects
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
      return render(request, 'welcome.html')
 
+def search_results(request):
+
+  if 'username' in request.GET and request.GET["projects"]:
+      category = request.GET.get("projects")
+      searched_categories = Projects.search_image(category)
+      message = f"{category}"
+
+      return render(request, 'all-ap/search.html',{"message":message,"projects": searched_categories})
+
+  else:
+      message = " Found 0 images for the search term"
+      return render(request, 'all-ap/search.html',{"message":message})
