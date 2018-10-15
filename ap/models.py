22f2348e9d1de41964b0fb497c244c3e2cd09c6a
Edith <you@example.com>
2018-10-15 15:41:26 +0300

    created functions and methods for class projects

--- ap/models.py
+++ ap/models.py
@@ -3,10 +3,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Projects(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key = True)
     title = models.TextField(max_length = 20, default="title")
     landing_page_image = models.ImageField(upload_to = 'images/')
     description = models.TextField()
-    link_to_live_site = models.URLField(max_length = 150)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+@classmethod
+def search_projects(cls , search_term):
+    projects = cls.objects.filter( first_name__icontains = search_term )
+    return projects
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key = True)
     first_name = models.CharField(max_length=30)
@@ -15,3 +30,4 @@ class Profile(models.Model):
     bio = models.CharField(max_length=200)
     projects = models.ForeignKey(Projects, null=True)
     user_contact_info=models.IntegerField(default=0)
+
