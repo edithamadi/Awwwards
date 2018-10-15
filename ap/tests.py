a5b84c5c389aeca561bfc4c38836083a4dce552c
Edith <you@example.com>
2018-10-16 01:18:11 +0300

    tested project search function

--- ap/tests.py
+++ ap/tests.py
@@ -23,3 +23,35 @@ class ProfileTestClass(TestCase):
             self.profile.save_profile()
             profiles = Profile.objects.all()
             self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        """
+        Function testing that profile can be deleted
+        """
+        
+        self.profile.save_profile()
+        self.profile.delete_profile()
+
+    def search_profile(self):
+        """
+        Function to test if one can search a profile
+        """
+        self.profile.search_profile()
+        first_name= self.profile.search_profile(self.profile.user)
+        profile = Profile.objects.get(user_id=self.profile.user)
+        self.assertTrue(first_name, profile)
+
+class ProjectsTestClass(TestCase):
+    """
+    Testing projects class and its functions
+    """
+    def setUp(self):
+
+        self.user = User.objects.create(username='stella')
+        #creating an new profile
+        self.projects = Projects(landing_page_image='rock5.jpg', title='This is the title', description="this is the description",user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.projects, Projects))
+
+
